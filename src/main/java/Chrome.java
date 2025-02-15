31e422840a2cb08476dcb22f9b2607cd9d0d358e
AnotherSACE <e1456869@u.nus.edu>
2025-02-15 17:14:57 +0800

    Add Add and List methods Change logo to Chrome Rework previous methods for better compatability

--- src/main/java/Chrome.java
+++ src/main/java/Chrome.java
@@ -4,35 +4,64 @@ import java.util.Scanner;
 public class Chrome {
 
     static String line = "____________________________________________________________";
+    static String[] toDoList = new String[100];
+    static int currentIndex = 0;
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-
-        System.out.println(greet());
-        echo();
-        System.out.println(exit());
-
-    }
-
-    public static String greet(){
-        return line + "\nHello! I'm Chrome \n" +
-                "What can I do for you?\n" + line;
-    }
-
-    public static String exit(){
-        return line + "\nBye. Hope to see you again soon!\n" + line;
-    }
-
-    public static void echo(){
-        Scanner sc = new Scanner(System.in);
-        String sentence = sc.nextLine();
-        if (!sentence.equals("bye")) {
-            System.out.println(line + "\n" + sentence + "\n" + line);
-            echo();
+        String logo = " CCCC  H   H  RRRR   OOO   M   M  EEEEE \n"
+                + "C      H   H  R   R O   O  MM MM  E     \n"
+                + "C      HHHHH  RRRR  O   O  M M M  EEEE  \n"
+                + "C      H   H  R  R  O   O  M   M  E     \n"
+                + " CCCC  H   H  R   R  OOO   M   M  EEEEE \n";
+        System.out.println("Hello from\n\n" + logo);
+        greet();
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                exit();
+                break;
+            } else if (input.equals("list")) {
+                list();
+            } else {
+                add(input);
+            }
         }
     }
+
+    public static void greet(){
+        System.out.println(line + "\nHello! I'm Chrome \n" +
+                "What can I do for you?\n" + line);
+    }
+
+    public static void exit(){
+        System.out.println("\nBye. Hope to see you again soon!\n" + line);
+    }
+
+    public static void echo(String sentence){
+        if (!sentence.equals("bye")) {
+            System.out.println(line + "\n" + sentence + "\n" + line);
+        }
+    }
+
+    public static void add(String task){
+        if (currentIndex < 100) {
+            toDoList[currentIndex] = task;
+            System.out.println(line + "\nadded: " + task + "\n" + line);
+            currentIndex++;
+        } else {
+            System.out.println(line + "\nThis isn't supposed to happen\n" + line);
+        }
+    }
+
+    public static void list(){
+        System.out.println(line);
+        for(int i = 0; i < toDoList.length; i++){
+            if (toDoList[i] != null) {
+                System.out.println((i + 1) + ". " + toDoList[i]);
+            }
+        }
+        System.out.println(line);
+    }
+
 }
